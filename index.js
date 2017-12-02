5adae756bcedb2a5f52be98f5c493beded432a21
Joven Macaldo <jmacaldo@me.com>
2017-12-01 20:20:03 -0500

    config

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+var pg = require('pg');
+var parseConnectionString = require('pg-connection-string');
+const configuration = 'postgres://' + process.env.POSTGRES_USER + ':' + process.env.POSTGRES_PASSWORD + '@localhost/blog';
+const pool = new pg.Pool(typeof configuration === 'string' ? parseConnectionString.parse(configuration) : configuration);
+
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/blog', function(req, res) {
+    pool.connect(function(err, client, done) {
+      client.query('select * from blog order by tstamp desc', function(err, result) {
+      res.render('blog', {result: result.rows});
+        done();
+        });
+    });
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
