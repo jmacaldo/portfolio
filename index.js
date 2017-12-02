b087f7adc6739b726b1e155c0b97e0f5546e65a7
Joven Macaldo <jmacaldo@me.com>
2017-12-01 20:24:38 -0500

    config

--- index.js
+++ index.js
@@ -2,16 +2,24 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 var pg = require('pg');
+var cool = require('cool-ascii-faces');
 var parseConnectionString = require('pg-connection-string');
 const configuration = 'postgres://' + process.env.POSTGRES_USER + ':' + process.env.POSTGRES_PASSWORD + '@localhost/blog';
 const pool = new pg.Pool(typeof configuration === 'string' ? parseConnectionString.parse(configuration) : configuration);
+var app = express();
+var parser = require('body-parser');
 
+app.use(parser.json());
+app.use(parser.urlencoded({ extended: true }));
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/cool', function(request, response) {
+    response.send(cool());
+  })
   .get('/blog', function(req, res) {
     pool.connect(function(err, client, done) {
       client.query('select * from blog order by tstamp desc', function(err, result) {
