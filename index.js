3148a8578b0377f75250b7806756bbfb37c8b246
Joven Macaldo <jmacaldo@me.com>
2017-12-01 20:29:23 -0500

    config

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-var pg = require('pg');
-var cool = require('cool-ascii-faces');
-var parseConnectionString = require('pg-connection-string');
-const configuration = 'postgres://' + process.env.POSTGRES_USER + ':' + process.env.POSTGRES_PASSWORD + '@localhost/blog';
-const pool = new pg.Pool(typeof configuration === 'string' ? parseConnectionString.parse(configuration) : configuration);
+
+//custom vars
+var fs = require('fs');
 var app = express();
 var parser = require('body-parser');
-
+var pg = require('pg');
+var parseConnectionString = require('pg-connection-string');
+var cool = require('cool-ascii-faces');
 app.use(parser.json());
 app.use(parser.urlencoded({ extended: true }));
+const configuration = 'postgres://' + process.env.POSTGRES_USER + ':' + process.env.POSTGRES_PASSWORD + '@localhost/blog';
+const pool = new pg.Pool(typeof configuration === 'string' ? parseConnectionString.parse(configuration) : configuration);
+
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
